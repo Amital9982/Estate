c05cdf2b6d1ad84d628ed8fdc5962f9f14f7529f
arpit <arpitmittal050603@gmail.com>
2023-12-13 13:42:18 +0530

    MVC pattern setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 try {
   mongoose.connect(process.env.MONGO_URL);
@@ -11,10 +12,9 @@ try {
 const app = express();
 
 const PORT = "4000";
-app.use("/", (req, res) => {
-  res.send("hello from the server");
-});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
-  console.log("Server is runnig at", PORT);
 });
+
+app.use("/backend/user", userRouter);
