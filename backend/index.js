699cd9568ec8bfbba66f2720f584f4457737e43d
arpit <arpitmittal050603@gmail.com>
2023-12-13 11:35:34 +0530

    Database migration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+try {
+  mongoose.connect(process.env.MONGO_URL);
+  console.log("connected successfully");
+} catch (err) {
+  console.log("Database connection error: " + err);
+}
 const app = express();
-const PORT = "3000";
+
+const PORT = "4000";
 app.use("/", (req, res) => {
   res.send("hello from the server");
 });
