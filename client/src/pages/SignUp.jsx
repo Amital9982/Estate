d04803b9894379dda168954437c709eb357dd694
arpit <arpitmittal050603@gmail.com>
2023-12-18 22:48:03 +0530

    signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignOut</div>;
+  return <div>
+    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+    <form className="flex flex-col gap" >
+      <input type="text" placeholder="username"
+        className="border p-3 rounded-lg" id="username" />
+      <input type="email" placeholder="email"
+        className="border p-3 rounded-lg" id="email" />
+      <input type="password" placeholder="password"
+        className="border p-3 rounded-lg" id="username" />
+      <input type="password" placeholder="confirm password"
+        className="border p-3 rounded-lg" id="username" />
+
+    </form>
+
+  </div>;
 };
 
 export default SignUp;
